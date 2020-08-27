a7cf1558e0963b775e7ed74a06018638551a58ae
ohikkoshi <ohikkoshi@icloud.com>
2020-08-27 12:52:17 +0900

    プロジェクト作成

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import {
+	Container,
+	Header, Content, Footer,
+	Title, Left, Right, Body,
+	Button, Icon, Text, Form, Input, Item, Label, Card, CardItem, Thumbnail,
+} from 'native-base';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const _HEADER_ = "ヘッダー";
+const _FOOTER_ = "フッター";
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<Container style={styles.container}>
+				{/* ヘッダー */}
+				<Header style={styles.header}>
+					<Left>
+						<Button transparent onPress={() => { }}>
+							<Icon name="arrow-back" />
+							<Text>Back</Text>
+						</Button>
+					</Left>
+					<Title>{_HEADER_}</Title>
+					<Right>
+						<Button transparent onPress={() => { }}>
+							<Icon name="menu" />
+						</Button>
+					</Right>
+				</Header>
+
+				{/* コンテンツ */}
+				<Content style={styles.content}>
+					<Form>
+						<Item fixedLabel>
+							<Label>ラベル</Label>
+							<Input placeholder="example" />
+						</Item>
+					</Form>
+
+					<Button>
+						<Text>ボタン</Text>
+					</Button>
+
+					<Card>
+						<CardItem>
+							<Left>
+								<Body>
+									<Thumbnail source={require("./assets/favicon.png")} />
+									<Text>カード</Text>
+								</Body>
+							</Left>
+							<Text>テキスト</Text>
+						</CardItem>
+					</Card>
+				</Content>
+
+				{/* フッター */}
+				<Footer styles={styles.fotter}>
+					<Text>{_FOOTER_}</Text>
+				</Footer>
+			</Container>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		width: "100%",
+		height: "100%",
+	},
+	header: {
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	fotter: {
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	content: {
+		padding: 10,
+	},
 });
